5bd660d8f99f3e9e75b36ce42bad4ccf1f3f8c30
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 16:14:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,6 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    puts @@genres
     @@genres.each do |genre|
       if genre_count.keys.include?(genre)
         genre_count[genre] += 1
