b2d6fc253d82d1a3e2c342abe7971d4f29c8aa42
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 16:15:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,14 @@ class Song
   end
 
   def self.artist_count
-    @@artists.count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.keys.include?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
   end
 
 end
