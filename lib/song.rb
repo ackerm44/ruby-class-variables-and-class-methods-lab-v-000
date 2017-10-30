15adc0e769063c61c59f7588ce6eaf9081dbb955
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 16:06:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,10 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre_count[genre] = 1
+      if genre_count.keys != genre
+        genre_count = {genre: 1}
+      else
+        genre_count[genre].value += 1
     end
   end
 
